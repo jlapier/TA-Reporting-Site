98fcd698b55b1198385665bdfc1156adea6c4b74
Jeremiah Heller <jeremiah@inertialbit.net>
2010-07-23 01:55:12 +0800

    stub reports controller; install to_csv plugin (depends on fastercsv gem); basic csv export of all activities to reports#index

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   config.gem 'formtastic'
   config.gem 'calendar_date_select'
   config.gem "rich-acts_as_revisable", :lib => "acts_as_revisable", :source => "http://gems.github.com"
+  config.gem "fastercsv"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
