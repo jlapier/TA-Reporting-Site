ec6a3208ebfac3652ea5a130dc662396721cf433
Jeremiah Heller <jeremiah@inertialbit.net>
2010-07-22 08:40:47 +0800

    update/delete states

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -12,4 +12,32 @@ class StatesController < ApplicationController
     @state = State.find(params[:id])
     @regions = State.regions
   end
+  
+  def update
+    @state = State.find(params[:id])
+    if @state.update_attributes(params[:state])
+      flash[:notice] = "State successfully updated."
+      redirect_to states_path
+    else
+      flash[:warning] = "Please correct any errors and try again."
+      render :edit
+    end
+  end
+  
+  def create
+    @state = State.new(params[:state])
+    if @state.save
+      flash[:notice] = "State created successfully."
+      redirect_to states_path
+    else
+      flash[:warning] = "PLease correct any errors and try again."
+      render :new
+    end
+  end
+  
+  def destroy
+    states = State.destroy(params[:id] || params[:ids])
+    flash[:notice] = "Deleted #{@template.pluralize(states.size, 'state')}."
+    redirect_to states_path
+  end
 end
--- spec/controllers/states_controller_spec.rb
+++ spec/controllers/states_controller_spec.rb
@@ -53,5 +53,113 @@ describe StatesController do
       response.should render_template('states/edit')
     end
   end
+  
+  describe ":update, :id => integer, :state => {}" do
+    before(:each) do
+      @state = mock_model(State, {
+        :update_attributes => nil
+      })
+      State.stub(:find).and_return(@state)
+    end
+    it "loads a state as @state" do
+      State.should_receive(:find).and_return(@state)
+      put :update, :id => 1
+      assigns[:state].should == @state
+    end
+    it "updates @state from params[:state]" do
+      @state.should_receive(:update_attributes).with({
+        'name' => 'Delware',
+        'region_id' => 1
+      })
+      put :update, :id => 1, :state => {:name => 'Delware', :region_id => 1}
+    end
+    context "update succeeds :)" do
+      before(:each) do
+        @state.stub(:update_attributes).and_return(true)
+      end
+      it "sets a flash[:notice]" do
+        put :update, :id => 1
+        flash[:notice].should_not be_nil
+      end
+      it "redirects to the states page" do
+        put :update, :id => 1
+        response.should redirect_to states_path
+      end
+    end
+    context "update fails :(" do
+      before(:each) do
+        @state.stub(:update_attributes).and_return(false)
+      end
+      it "sets a flash[:warning]" do
+        put :update, :id => 1
+        flash[:warning].should_not be_nil
+      end
+      it "renderes the edit template" do
+        put :update, :id => 1
+        response.should render_template('states/edit')
+      end
+    end
+  end
+  
+  describe ":create, :state => {}" do
+    before(:each) do
+      @state = mock_model(State, {
+        :save => nil
+      }).as_new_record
+      State.stub(:new).and_return(@state)
+    end
+    it "instantiates a new state as @state" do
+      State.should_receive(:new).and_return(@state)
+      post :create
+      assigns[:state].should == @state
+    end
+    it "saves the new @state" do
+      @state.should_receive(:save)
+      post :create
+    end
+    context "save succeeds" do
+      before(:each) do
+        @state.stub(:save).and_return(true)
+      end
+      it "sets a flash[:notice]" do
+        post :create
+        flash[:notice].should_not be_nil
+      end
+      it "redirects to the states page" do
+        post :create
+        response.should redirect_to states_path
+      end
+    end
+    context "save fails :(" do
+      before(:each) do
+        @state.stub(:save).and_return(false)
+      end
+      it "sets a flash[:warning]" do
+        post :create
+        flash[:warning].should_not be_nil
+      end
+      it "renders the new template" do
+        post :create
+        response.should render_template('states/new')
+      end
+    end
+  end
+  
+  describe ":destroy, :id(s) => integer or [int1,int2]" do
+    before(:each) do
+      @state = mock_model(State, {
+        :size => 1
+      })
+      State.stub(:destroy).and_return(@state)
+    end
+    it "destroys the state(s) with the given id(s)" do
+      State.should_receive(:destroy).with('1')
+      delete :destroy, :id => 1
+    end
+    it "redirects to the states page" do
+      delete :destroy, :id => 1
+      response.should redirect_to states_path
+    end
+  end
 
 end
