29c6ff57a451a51b25bf67ccdc75b3bbe920c9de
Jeremiah Heller <jeremiah@inertialbit.net>
2010-08-09 11:36:47 -0700

    red > green

--- spec/views/summary_reports/show.html.erb_spec.rb
+++ spec/views/summary_reports/show.html.erb_spec.rb
@@ -3,10 +3,8 @@ require 'spec_helper'
 describe "/summary_reports/show.html.erb" do
   include SummaryReportsHelper
   before(:each) do
-    assigns[:summary_report] = @summary_report = stub_model(SummaryReport,
-      :name => "some report name",
-      :end_period => Date.new(2010, 5)
-    )
+    assigns[:summary_report] = @summary_report = mock_model(SummaryReport).as_null_object
+    assigns[:intensity_levels] = []
   end
 
   it "renders attributes in <p>" do
