0e7fd82b7aa7d2f1c9f6627f1b099c60de2d8a2c
Jason LaPier <jason.lapier@gmail.com>
2011-02-17 10:47:01 -0800

    trying to get rid of deprecation messages but failed

--- spec/views/activities/index.html.erb_spec.rb
+++ spec/views/activities/index.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "/activities/index" do
   before(:each) do
     mock_objective = mock_model(Objective, {
-        :name => "Obj 1", :display_name => "Obj 1", :number => 1 })
+        :name => "Obj 1", :display_name => "Obj 1", :number => 1, :id => 99 })
 
     assigns[:objectives] = [mock_objective]
     assigns[:ta_delivery_methods] = []
@@ -13,6 +13,7 @@ describe "/activities/index" do
 
     assigns[:activities] = [
       mock_model(Activity, {
+        :id => 999,
         :description => 'Activity',
         :date_of_activity => 4.days.ago,
         :objective_id => mock_objective.id,
